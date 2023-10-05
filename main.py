f9707893287c46eb3b9b681c355333834b55d09f
Sahil <sahilismailnavar@gmail.com>
2023-10-05 17:13:48 +0530

    Added time taking to crack a password

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 from random import *
+import time
 
 u_pwd = input("Enter a Password : ")
-PWD = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9']
+PWD = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+       'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# If You want add special character in the above list
 
 pw = ""
+start_time = time.time()
 
 while pw != u_pwd:
     pw = ""
@@ -17,6 +21,8 @@ while pw != u_pwd:
         print(pw)
         print("Cracking Password .... Please Wait !!!")
 
-print(f"\nYour password is : {pw} \n")
+end_time = time.time()
+time_taken = end_time - start_time
 
-# ! LET'S CRACK IT
+print(f"\nYour password is : {pw}")
+print(f"\nTime taken to Crack the password: {time_taken:.2f} seconds\n")
